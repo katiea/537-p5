71fae3255685d85dce2f504eb2c1494bc1dd566a
Ram Bamdhamravuri <bamdhamr@mumble-31.cs.wisc.edu>
2014-05-03 21:01:32 -0500

    extracted port, diskimage from  the command line in server.c

--- server.c
+++ server.c
@@ -5,10 +5,35 @@
 
 #define BUFFER_SIZE (4096)
 
+
+int port;
+char *image;
+
+void getargs(int *port, char *image, int argc, char *argv[]) {
+
+    if (argc != 3) {
+	fprintf(stderr, "Usage0: %s [port] [disk-image] \n", argv[0]);
+	exit(1);
+    }
+    *port = atoi(argv[1]);
+    strcpy(image, argv[2]);
+
+}
+
+
+
+
+
+
+
+
 int
 main(int argc, char *argv[])
 {
-    int sd = UDP_Open(10000);
+   
+     image = malloc(8096);
+    getargs(&port,image,argc,argv);
+    int sd = UDP_Open(port);
     assert(sd > -1);
 
     printf("                                SERVER:: waiting in loop\n");
